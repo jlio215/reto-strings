5807e2d14511d60a0184b72950f8d827a30dbc06
jlio215 <31058099+jlio215@users.noreply.github.com>
2022-07-05 02:01:44 -0500

    Add files via upload

---
+++ new.py
@@ -0,0 +1,30 @@
+
+a=input()
+b=input()
+c=input()
+p1=0
+p2=0
+e=""
+for i in c:
+    if i in a:
+        p1 +=1
+    if i in b:
+        p2 +=1
+    if p1>p2:
+        e +="1"
+    elif p2>p1:
+        e +="2"
+    elif p1==p2:
+        e +="*"
+print(e)    
+
+#QCWKFI
+
+#LGECUO
+
+#KATOAETFQUAPMVKLGMT
+
+#MHJW
+
+#PMTZ
+#PYAFKHRWZOUYMJPMQM
